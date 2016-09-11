105088945fa833ca2f34acca315d3589d17c1101
sabisambra <s.abisambra125@uniandes.edu.co>
2016-09-11 16:25:05 -0500

    Cambios Reconocer voz

--- Appzheimer/app/src/main/java/moviles/appzheimer/MainActivity.java
+++ Appzheimer/app/src/main/java/moviles/appzheimer/MainActivity.java
@@ -1,10 +1,14 @@
 package moviles.appzheimer;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void reconocerVoz(View v)
     {
-
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Voice Recognition Demo...");
+        startActivityForResult(intent,REQUEST_CODE);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+
+        }
+    }
+
 
 }
