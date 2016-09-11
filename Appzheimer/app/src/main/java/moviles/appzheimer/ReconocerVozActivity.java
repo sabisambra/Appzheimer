026614961c27e8f16ba29d1641138ccdf1f38bd3
sabisambra <s.abisambra125@uniandes.edu.co>
2016-09-11 15:47:16 -0500

    Intento Reconocimiento

--- Appzheimer/app/src/main/java/moviles/appzheimer/ReconocerVozActivity.java
+++ Appzheimer/app/src/main/java/moviles/appzheimer/ReconocerVozActivity.java
@@ -2,19 +2,12 @@ package moviles.appzheimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ReconocerVozActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reconocer_voz);
     }
-
-    public void reconocerVoz(View v)
-    {
-
-    }
-
 }
