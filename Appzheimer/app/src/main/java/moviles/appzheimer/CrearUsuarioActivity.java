f089ef77f9452dfc35ecf352d5cca5e7dfeb652c
sabisambra <s.abisambra125@uniandes.edu.co>
2016-09-11 19:01:48 -0500

    Crear Usuario

---
+++ Appzheimer/app/src/main/java/moviles/appzheimer/CrearUsuarioActivity.java
@@ -0,0 +1,19 @@
+package moviles.appzheimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CrearUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_usuario);
+    }
+
+    public void crearUsuario(View v)
+    {
+
+    }
+}
--- Appzheimer/app/src/main/java/mundo/Usuario.java
+++ Appzheimer/app/src/main/java/mundo/Usuario.java
@@ -42,34 +42,74 @@ public class Usuario {
         direccion = pDireccion;
     }
 
+    /**
+     * Metodo constructor sin parametros
+     */
+    private Usuario()
+    {
+
+    }
+
+    /**
+     * Metodo que retorna la edad del usuario
+     * @return la edad del usuario
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * Metodo que cambia la edad del usuario
+     * @param pEdad la edad del usuario
+     */
     public void setEdad(int pEdad) {
         edad = pEdad;
     }
 
+    /**
+     * Metodo que retorna el nombre del usuario
+     * @return el nombre del usuario
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo que cambia el nombre del usuario
+     * @param pNombre el nombre del usuario
+     */
     public void setNombre(String pNombre) {
         nombre = pNombre;
     }
 
+    /**
+     * Metodo que retorna la fecha de nacimiento del usuario
+     * @return la fecha de nacimiento del usuario
+     */
     public Date getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * Metodo que cambia la fecha de nacimiento del usuario
+     * @param pFechaNacimiento
+     */
     public void setFechaNacimiento(Date pFechaNacimiento) {
         fechaNacimiento = pFechaNacimiento;
     }
 
+    /**
+     * Metodo que retorna la direccion del usuario
+     * @return la direccion del usuario
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * Metodo que cambia la direccion del usuario
+     * @param pDireccion la direccion del usuario
+     */
     public void setDireccion(String pDireccion) {
         direccion = pDireccion;
     }
