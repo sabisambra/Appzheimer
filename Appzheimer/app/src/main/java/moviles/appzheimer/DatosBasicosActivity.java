f59a076fd3de38522914acfed13f0499df7edef9
sabisambra <s.abisambra125@uniandes.edu.co>
2016-09-17 23:15:59 -0500

    finalizar datos basicos

--- Appzheimer/app/src/main/java/moviles/appzheimer/DatosBasicosActivity.java
+++ Appzheimer/app/src/main/java/moviles/appzheimer/DatosBasicosActivity.java
@@ -1,15 +1,25 @@
 package moviles.appzheimer;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 
 public class DatosBasicosActivity extends AppCompatActivity {
+
+    /**
+     * El calendario para poder traer los datos de la fecha
+     */
+    private Calendar calendario;
+
+    private String diasSemana[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,8 @@ public class DatosBasicosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_datos_basicos);
         TextView nombre = (TextView) findViewById(R.id.textNombreUsuarioDatosBasicos);
         TextView edad = (TextView) findViewById(R.id.textEdadDatosBasicos);
+        TextView direccion = (TextView) findViewById(R.id.textDireccionDatosBasicos);
+        TextView diaFecha = (TextView) findViewById(R.id.textDiaFechaDatosBasicos);
         try
         {
             InputStream archivo = openFileInput(CrearUsuarioActivity.DATOSUSUARIO);
@@ -26,11 +38,35 @@ public class DatosBasicosActivity extends AppCompatActivity {
                 BufferedReader lector = new BufferedReader(temp);
                 nombre.setText(lector.readLine());
                 edad.setText(lector.readLine());
+                //Esta linea contiene la fecha de nacimiento del paciente
+                lector.readLine();
+                direccion.setText(lector.readLine());
+                lector.close();
             }
         }
         catch(Exception e)
         {
             new AlertDialog.Builder(this).setTitle("Error").setMessage(e.getMessage()).setNeutralButton("Cerrar", null).show();
         }
+        diasSemana = new String[7];
+        diasSemana[0] = "Lunes";
+        diasSemana[1] = "Martes";
+        diasSemana[2] = "Miercoles";
+        diasSemana[3] = "Jueves";
+        diasSemana[4] = "Viernes";
+        diasSemana[5] = "Sabado";
+        diasSemana[6] = "Domingo";
+        calendario = Calendar.getInstance();
+        diaFecha.setText(diasSemana[calendario.get(Calendar.DAY_OF_WEEK)-1] + " " + calendario.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * Metodo para volver a la ventanta principal
+     * @param v la vista principal
+     */
+    public void volverMenuPrinciapl(View v)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
